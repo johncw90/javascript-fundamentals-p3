d82779613a90daffd727674c9276dd3a9923bf2b
John <johncw90@yahoo.com>
2023-09-10 17:59:59 -0500

    Finish exercises

---
+++ script.js
@@ -0,0 +1,7 @@
+// const add7 = (num) => num + 7;
+
+// const multiply = (n1, n2) => n1 * n2;
+
+// const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const lastLetter = (str) => str.charAt(str.length - 1);
